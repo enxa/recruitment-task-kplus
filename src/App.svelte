c0ac0ce155023fe228cbbb6ad5f5852a61c12da0
enxa <cezary.walczak.pl@gmail.com>
2022-07-01 21:40:49 +0200

    Add all tasks

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,36 @@
+<script>
+	import { fadeIn, handleClick } from './index.js'
+	
+	let imgs = [
+		"assets/img/1.jpg",
+		"assets/img/2.jpg",
+		"assets/img/3.jpg",
+		"assets/img/3.jpg",
+		"assets/img/2.jpg",
+		"assets/img/1.jpg",
+		"assets/img/1.jpg",
+		"assets/img/2.jpg",
+		"assets/img/3.jpg",
+	]
+</script>
+
 <div id="app" class="container">
-	<div class="wrap">
-		<img src="assets/img/1.jpg" alt="img">
-		<img src="assets/img/2.jpg" alt="img">
-		<img src="assets/img/3.jpg" alt="img">
-		<img src="assets/img/3.jpg" alt="img">
-		<img src="assets/img/2.jpg" alt="img">
-		<img src="assets/img/1.jpg" alt="img">
-		<img src="assets/img/1.jpg" alt="img">
-		<img src="assets/img/2.jpg" alt="img">
-		<img src="assets/img/3.jpg" alt="img">
+	<div class="wrap" use:fadeIn>
+		{#each imgs as img, i}
+			<img src={img} alt="img {++i}" class={`img${i}`} data-delay="{i}" use:handleClick/>
+		{/each}
 	</div>
 </div>
 
 <style lang="scss">
 	@import '../public/assets/styles/index.scss';
+
+	#app {
+		.wrap {
+			img:nth-child(3) { 
+				animation: heartbeat 1s infinite;
+			}
+		}
+	}
+	
 </style>
